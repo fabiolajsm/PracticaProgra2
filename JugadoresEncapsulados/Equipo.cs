cf71f4bed95c740d7be99b324823ac2836e55502
Dario Jejer <suarezfabiola17@gmail.com>
2023-04-22 11:26:40 -0300

    Ejercicio Estadistica deportiva antes de hacer el de jugadores encapsulados

---
+++ JugadoresEncapsulados/Equipo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugadoresEncapsulados
+{
+    public class Equipo
+    {
+        public short cantidadDeJugadores;
+        public string nombre;
+        public List<Jugador> jugadores;
+
+        private Equipo() {         
+            this.jugadores = new List<Jugador>();
+        }
+        public Equipo(short cantidad, string nombre): this() { 
+            this.cantidadDeJugadores = cantidad;
+            this.nombre = nombre;
+        }
+
+        // Sobrecarga: el operador + agregará jugadores a la lista siempre y cuando no exista aún en el equipo y la cantidad de jugadores no supere el límite establecido por el atributo cantidadDeJugadores.
+        public static bool operator +(Equipo equipo, Jugador jugador) { 
+            if(equipo.jugadores.Count == 0 &&  equipo.jugadores.Count <= equipo.cantidadDeJugadores)
+            {
+                foreach (Jugador item in equipo.jugadores)
+                {
+                    if(item == jugador) // esta es la sobrecarga que esta en jugador
+                    {
+                        return false;
+                    }
+                }
+                equipo.jugadores.Add(jugador);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ JugadoresEncapsulados/Jugador.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JugadoresEncapsulados
+{
+    public class Jugador
+    {
+        private int dni;
+        private string nombre;
+        private int partidosJugados;
+        private float promedioGoles;
+        private int totalGoles;
+
+        private Jugador() {
+            // Todos los datos estadísticos del jugador se inicializarán en 0 dentro del constructor privado.
+            this.partidosJugados = 0;
+            this.promedioGoles = 0;
+            this.totalGoles = 0;
+        }
+        public Jugador(int dni, string nombre) : this()
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+        }
+
+        public Jugador(int dni, string nombre, int totalGoles, int partidosJugados)
+        {
+            this.dni = dni;
+            this.nombre = nombre;
+            this.totalGoles = totalGoles;
+            this.partidosJugados = partidosJugados;
+        }
+
+        public float GetPromedioGoles() {
+            return (float)this.partidosJugados / this.totalGoles;
+        }
+        public string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Nombre : {this.nombre}, DNI: {this.dni.ToString()}");
+            sb.Append($"Partidos Jugados: {this.partidosJugados.ToString()}, Promedio Goles: {GetPromedioGoles().ToString()}");
+            sb.Append($"Total goles: {this.totalGoles.ToString()}");
+            return sb.ToString();
+        }
+
+        // Sobrecarga: Dos jugadores seran iguales si tienen el mismo DNI
+        public static bool operator == (Jugador uno, Jugador dos){
+            return uno.dni == dos.dni;
+        }
+        public static bool operator !=(Jugador uno, Jugador dos)
+        {
+            return uno.dni != dos.dni;
+        }
+    }
+}
